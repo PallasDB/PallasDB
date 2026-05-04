6ac158172076e6fe54fddf83ae8b5d64e604dea8
Teddy Malhan <ama367@sfu.ca>
2026-05-03 23:18:26 -0700

    feat: implement 0101

--- db_project/0101/kv.go
+++ db_project/0101/kv.go
@@ -11,10 +11,27 @@ func (kv *KV) Open() error {
 
 func (kv *KV) Close() error { return nil }
 
-func (kv *KV) Get(key []byte) (val []byte, ok bool, err error)
+func (kv *KV) Get(key []byte) (val []byte, ok bool, err error) {
+	x, ok := kv.mem[string(key)]
+	return x, ok, nil
+}
 
-func (kv *KV) Set(key []byte, val []byte) (updated bool, err error)
+func (kv *KV) Set(key []byte, val []byte) (updated bool, err error) {
+	_, exists := kv.mem[string(key)]
+	if !exists {
+		updated = true
+	}
+	kv.mem[string(key)] = val
+	return updated, nil
+}
 
-func (kv *KV) Del(key []byte) (deleted bool, err error)
+func (kv *KV) Del(key []byte) (deleted bool, err error) {
+	_, exists := kv.mem[string(key)]
+	if exists {
+		delete(kv.mem, string(key))
+		deleted = true
+	}
+	return deleted, nil
+}
 
 // QzBQWVJJOUhU https://trialofcode.org/
