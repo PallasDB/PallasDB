e99a6ff53eb4f4eb30c59808dcca450b7d4e97ce
Teddy Malhan <ama367@sfu.ca>
2026-05-10 21:36:04 -0700

    feat: implement 0303 parse SELECT (parseEqual, parseSelect, parseWhere)

--- db_project/0303/sql_parser.go
+++ db_project/0303/sql_parser.go
@@ -147,11 +147,60 @@ func (p *Parser) parseInt(out *Cell) (err error) {
 	return nil
 }
 
-func (p *Parser) parseEqual(out *NamedCell) error
+func (p *Parser) parseEqual(out *NamedCell) error {
+	var ok bool
+	out.column, ok = p.tryName()
+	if !ok {
+		return errors.New("expect column")
+	}
+	if !p.tryPunctuation("=") {
+		return errors.New("expect =")
+	}
+	return p.parseValue(&out.value)
+}
 
-func (p *Parser) parseSelect(out *StmtSelect) error
+func (p *Parser) parseSelect(out *StmtSelect) error {
+	if !p.tryKeyword("SELECT") {
+		return errors.New("expect keyword")
+	}
+	for !p.tryKeyword("FROM") {
+		if len(out.cols) > 0 && !p.tryPunctuation(",") {
+			return errors.New("expect comma")
+		}
+		if name, ok := p.tryName(); ok {
+			out.cols = append(out.cols, name)
+		} else {
+			return errors.New("expect column")
+		}
+	}
+	if len(out.cols) == 0 {
+		return errors.New("expect column list")
+	}
+	var ok bool
+	if out.table, ok = p.tryName(); !ok {
+		return errors.New("expect table name")
+	}
+	return p.parseWhere(&out.keys)
+}
 
-func (p *Parser) parseWhere(out *[]NamedCell) error
+func (p *Parser) parseWhere(out *[]NamedCell) error {
+	if !p.tryKeyword("WHERE") {
+		p.tryPunctuation(";")
+		return nil
+	}
+	for {
+		nc := NamedCell{}
+		if err := p.parseEqual(&nc); err != nil {
+			return err
+		}
+		*out = append(*out, nc)
+		if !p.tryKeyword("AND") {
+			break
+		}
+	}
+	p.tryPunctuation(";")
+	return nil
+}
 
 func (p *Parser) isEnd() bool {
 	p.skipSpaces()
